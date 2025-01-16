6f2f5ff4b0a65d5bb3c40a8ff3f63b3dc8260102
LostRishi <dps.raj2011@gmail.com>
2025-01-16 17:56:19 +0530

    refactored code

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-import numpy as np 
+# Step 1: Import Libraries and Load the Model
+import numpy as np
 import tensorflow as tf
-import streamlit as st
 from tensorflow.keras.datasets import imdb
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
 
-# load the IMDB dataset 
+# Load the IMDB dataset word index
 word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
-# Load the pre-trained model with Relu activation
+# Load the pre-trained model with ReLU activation
 model = load_model('simple_rnn.h5')
 
-# Step 2: Helper function
+# Step 2: Helper Functions
 # Function to decode reviews
-def decode_review(encoded_reviews):
-    return ' '.join([reverse_word_index.get(i - 3, '?') for i in encoded_reviews])
+def decode_review(encoded_review):
+    return ' '.join([reverse_word_index.get(i - 3, '?') for i in encoded_review])
 
-# Function to preproces user input
-def preproces_text(text):
+# Function to preprocess user input
+def preprocess_text(text):
     words = text.lower().split()
     encoded_review = [word_index.get(word, 2) + 3 for word in words]
     padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
     return padded_review
 
-# Step 4: Streamlit app
-st.title("IMDB Sentiment Analysis")
-st.write("Enter a movie review to predict its sentiment:")
 
-## User input
-user_input = st.text_area("Movie review")
+import streamlit as st
+## streamlit app
+# Streamlit app
+st.title('IMDB Movie Review Sentiment Analysis')
+st.write('Enter a movie review to classify it as positive or negative.')
+
+# User input
+user_input = st.text_area('Movie Review')
 
 if st.button('Classify'):
-    preprocess_input = preproces_text(user_input)
 
-    # Make prediction
-    prediction = model.predict(preprocess_input)
-    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+    preprocessed_input=preprocess_text(user_input)
 
-    # Display prediction
-    st.write(f"Sentiment: {sentiment}")
-    st.write(f"Prediction score: {prediction[0][0]}")
+    ## MAke prediction
+    prediction=model.predict(preprocessed_input)
+    sentiment='Positive' if prediction[0][0] > 0.5 else 'Negative'
 
-else :
-    st.write("Please enter a movie review to predict its sentiment.")
+    # Display the result
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction[0][0]}')
+else:
+    st.write('Please enter a movie review.')
